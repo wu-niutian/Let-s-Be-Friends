68b8045b62355345e83b0ea9088ee853b039a632
Wu Niutian <wuniutian@gmail.com>
2022-07-09 13:42:20 +0800

    Initial Commit

---
+++ Let's Be Friends/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Let's Be Friends
+//
+//  Created by    wuniutian on 9/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Let's Be Friends/Let_s_Be_FriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Let_s_Be_FriendsApp.swift
+//  Let's Be Friends
+//
+//  Created by    wuniutian on 9/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Let_s_Be_FriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
